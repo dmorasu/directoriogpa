974d626b38ac3d9b956c1a7835eaaff21d3c6e6c
Alejandro Mora <68551220+dmorasu@users.noreply.github.com>
2022-02-03 16:35:20 -0500

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,64 @@
+.title {
+    color:rgb(255, 255, 255);
+    
+}
+
+.tablacolor {
+  color: rgb(0, 0, 0);
+  font-weight: 400;
+  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+
+}
+
+.centrado{
+    text-align: left ;
+    
+  }
+
+  .relleno {
+    background: rgb(9, 136, 191);
+    color: whitesmoke;  
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  }
+
+  .relleno2 {
+     background: rgba(255, 255, 255, 0.877);
+    }
+  }
+
+  .negrita {
+    font-weight: 700;
+  }
+
+  .tamano {
+    font-size: 100%;
+  }
+
+  .fondo {
+     background-color: rgb(221, 244, 249);
+     background-repeat: no-repeat;
+  }
+
+  
+
+  .fondo2{
+    /* Aquí el origen de la imagen */
+    background-image: url(/imagenes/edificio2-min.jpg);
+  
+    /* Fijar la imagen de fondo este vertical y
+      horizontalmente y centrado */
+    background-position: center center;
+  
+    /* Esta imagen no debe de repetirse */
+    background-repeat: no-repeat;
+  
+    /* COn esta regla fijamos la imagen en la pantalla. */
+    background-attachment: fixed;
+  
+    /* La imagen ocupa el 100% y se reescala */
+    background-size: cover;
+  
+    /* Damos un color de fondo mientras la imagen está cargando  */
+    background-color: #a8a8a8;
+  }
